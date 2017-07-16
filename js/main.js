7cc6e698cd75aeb6ccfc61fbd624f829d8efd447
Letitbe133 <lionel.lft@me.com>
2017-07-16 23:10:08 +0200

    js functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 $(function(){
 
+  function setHeight() {
+    var hCard = $('.card:first .card-image').outerHeight();
+    var hImg = $('.card:first .card-image img').outerHeight();
+    var wImg = $('.card:first .card-image img').outerWidth();
+
+    $('#target').css("height", hCard);
+    $('#target iframe').attr({
+      height: hImg,
+      width: wImg
+    });
+  }
+
+  setHeight();
+
+  $(".button-collapse").sideNav({
+    menuWidth: 200,
+    closeOnClick: true
+  });
+
+  $('.scrollspy').scrollSpy({scrollOffset: 30});
 
 
-  
+
+
 })
