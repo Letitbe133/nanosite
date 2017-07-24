54d98e35e781c8ff099819fac271bc67ac5e9e9d
Letitbe133 <lionel.lft@me.com>
2017-07-24 21:44:21 +0200

    fixed contact form treatment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 
+// Egalisation hauteurs de cartes
   function setHeight() {
     var hCard = $('.card:first .card-image').outerHeight();
     var hImg = $('.card:first .card-image img').outerHeight();
@@ -14,11 +15,13 @@ $(function(){
 
   setHeight();
 
+// Initialisation sideNav
   $(".button-collapse").sideNav({
     menuWidth: 200,
     closeOnClick: true
   });
 
+// Initialisation scrollspy
   $('.scrollspy').scrollSpy({scrollOffset: 30});
 
 
@@ -31,5 +34,48 @@ $(function(){
   $('#modal-trigger').on('click', function(){
     $('#modal').modal('close');
   })
+
+  // // Initialisation toast
+  // $('button[type=submit]').on("click", function(){
+  //   setTimeout(function() {
+  //     var feedback = $('.feedback').text();
+  //     if(feedback.length > 0){
+  //       Materialize.toast(feedback, 7000);
+  //     }
+  //   }, 1000);
+  //   })
+
+  const MutationObserver = window.MutationObserver ||
+  window.WebKitMutationObserver ||
+  window.MozMutationObserver
+
+
+  var observer = new MutationObserver((changes) => {
+    console.log(changes);
+    changes.forEach((change) => {
+      console.log('Mutation', change);
+      if(change.type === 'characterData') {
+        console.info('text content', change.target.nodeValue)
+      }
+    });
+  });
+
+  var target = document.querySelector('#feedback');
+
+  var config = {
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree: true
+  };
+
+  observer.observe(target, config);
+
+
+
+
+
+
+
 
 })
