ff85dbc70b16388090c7960deff47390ca14e43d
mi <mi@localhost.localdomain>
2017-08-24 14:25:55 +0200

    modif traitement formulaire et alt

--- index.php
+++ index.php
@@ -49,18 +49,18 @@
         <div class="nav-wrapper cyan darken-3">
         <a href="#" data-activates="mobile-menu" class="button-collapse right"><i class="material-icons">menu</i></a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="#section1">C'est quoi ?</a></li>
-          <li><a href="#section2">Pourquoi ?</a></li>
+          <li><a href="#section1">Présentation</a></li>
+          <li><a href="#section2">Personnalisation</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
         <ul class="side-nav" id="mobile-menu">
-          <li><a href="#section1">C'est quoi ?</a></li>
-          <li><a href="#section2">Pourquoi ?</a></li>
+          <li><a href="#section1">Présentation</a></li>
+          <li><a href="#section2">Personnalisation</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
         </div>
       </nav>
-    </div>
+    </div><!--NAV-->
     <header>
     <div class="content">
         <div class="row">
@@ -80,17 +80,17 @@
       </div>
       <div class="overlay">
       </div>
-    </header>
+    </header><!--HEADER-->
 <section id="section1" class="scrollspy">
       <div class="row">
         <div class="col s12 m8 offset-m2 ">
           <h2 class="center-align grey-text">Le TinySite est : </h2>
         </div>
         <div class="separator col s2 offset-s5 divider"></div>
-        </div>
+        </div><!--TITLE-->
         <div class="row">
         <div id="cards" class="col s12 l10 offset-l1">
-          <div class="col s12 m4 ">
+          <div class="col s12 m8 offset-m2 l4 ">
             <div class="card">
               <div class="card-image orange lighten-2 waves-effect waves-block waves-light">
                 <img class="activator" src="assets/picto-pig.png" alt="Un site web abordable">
@@ -113,8 +113,8 @@
               <p>Le TinySite que nous réalisons pour vous vous appartient. Vous pouvez à tout moment récuperer le code pour vous occuper vous-même de sa maintenance. Nous vous proposons même <a href="http://www.cooldev.xyz/formation-web.php" target="_blank"> une formation </a>pour ça !
               </div>
             </div>
-          </div>
-        <div class="col s12 m4 ">
+          </div><!--CARD 1-->
+        <div class="col s12 m8 offset-m2 l4 ">
           <div class="card">
             <div class="card-image teal lighten-3 card-image waves-effect waves-block waves-light">
 					<img  class="activator" src="assets/picto-responsive.png" alt="Un site web adaptatif">
@@ -124,11 +124,11 @@
             </div>
             <div class="card-reveal">
               <h4 class="card-title">TinySite est responsive<i class="material-icons right">close</i></h4>
-              <p>La moitié des internautes vous cherche depuis un mobile. Le TinySite s'adapte à tous les supports.</p>
+              <p>La moitié des internautes vous cherche depuis un mobile. Le TinySite s'adapte à la majorité des supports web actuels. </p>
               </div>
           </div>
-        </div>
-        <div class="col s12 m4 ">
+        </div><!--CARD 2-->
+        <div class="col s12 m8 offset-m2 l4 ">
           <div class="card">
             <div class="card-image  red accent-2 waves-effect waves-block waves-light">
               <img class="activator" src="assets/picto-perso.png" alt="Un site web évolutif">
@@ -141,117 +141,92 @@
               <p>Votre site web vous suit ! Ajouter des sections, des pages...<br>Contactez-nous, nous vous proposerons des solutions adaptées.</p>
             </div>
           </div>
+      </div><!--CARD 3-->
       </div>
-      </div>
-		</div>
+		</div><!--CARDS-->
 
-</section>
+</section><!--SECTION 1-->
 
 <section id="section2" class="cyan darken-2 scrollspy">
 
 	 <div class="container">
 	 	<div class="row">
       	<div class="col s12 m8 offset-m2 white-text">
-        <h2 class="center-align">Un TinySite, ça ressemble à quoi ?</h2>
+        <h2 class="center-align">Un TinySite qui vous ressemble</h2>
         </div>
         <div class="separator col s2 offset-s5 divider"></div>
         <div class="col s12 center-align">
-        <img class="responsive-img" src="assets/structure-tinysite.png" alt="">
+        <img class="responsive-img" src="assets/structure-tinysite.png" alt="Un tinySite c'est un site web avec votre logo, 3 sections personnalisables et vos coordonnées ">
         </div>
-        <!--<div class="separator col s2 offset-s5 divider"></div>
-       </div>
-
-       <div class="row">
-			<div class="col s12 m6 l6 white-text">
-									<div class="center-align">
-
-				              <img width="80px;" src="assets/picto-shop-2.png" alt="Parlez de vous">
-									</div>
-
-				              <p>Où vous trouver, comment vous contacter, êtes-vous ouvert aujourd'hui ?<br/>
-				              Autant d'informations que vos futurs clients doivent trouver rapidement, parce que c'est ça qu'ils cherchent !
-				              </p>
-				          </div>
-				         	<div class="col s12 m6 l6 white-text">
-				            <div class="center-align">
-				              <img width="80px;" src="assets/picto-shop-2.png" alt="Parlez de ce que vous faites">
-				            </div>
-				            <div>
-				              <p>Que faites vous et pourquoi le faites-vous si bien ?<br/>
-				                 Du contenu de qualité favorise votre présence sur les moteurs de recherche et vos clients vous trouvent plus facilement !
-				                 Ca sert à cela un site vitrine :)
-				              </p>
-				            </div>
-				         	</div>-->
        </div>
       </div>
 
-</section>
+</section><!--SECTION 2-->
 
 <section id="contact" class="container scrollspy">
-          <div class="col s12">
-            <h2 class="dark-green center-align">Contactez-nous !</h2>
-            <div class="col s12"></div>
-          </div>
-
-        <div class="row">
-          <form class="col s12" action="contact.php" method="POST">
-
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">account_circle</i>
-                <input placeholder="John Doe" id="name" name="nom" type="text" class="validate" required>
-                <label for="name">Votre nom</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">email</i>
-                <input placeholder="john.doe@mail.com" id="email" name="email" type="email" class="validate" required>
-                <label for="email">Votre adresse mail</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">subject</i>
-                <input placeholder="Objet de votre message" id="subject" name="objet" type="text" class="validate" required>
-                <label for="subject">Sujet</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s12">
-                <i class="material-icons prefix">textsms</i>
-                <textarea placeholder="Votre message" id="message" name="message" type="text" class="materialize-textarea" required></textarea>
-                <label for="message">Votre message</label>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col s12 center-align">
-                <button class="btn waves-effect waves-light cyan darken-3" id="submit" type="submit" name="submit">Envoyer
-                  <i class="material-icons right">send</i>
-                </button>
-              </div>
-            </div>
-            <div id="feedback">
-              <?php echo $feedback; ?>
-            </div>
-          </form>
-
-        </div>
-      </section>
-       <footer class="page-footer cyan darken-2 ">
+	    <div class="col s12">
+	      <h2 class="dark-green center-align">Contactez-nous !</h2>
+	      <div class="col s12"></div>
+	    </div>
+	
+	  <div class="row">
+	    <form class="col s12" action="contact.php" method="POST">
+	
+	      <div class="row">
+	        <div class="input-field col s12">
+	          <i class="material-icons prefix">account_circle</i>
+	          <input placeholder="John Doe" id="name" name="nom" type="text" class="validate" required>
+	          <label for="name">Votre nom</label>
+	        </div>
+	      </div>
+	      <div class="row">
+	        <div class="input-field col s12">
+	          <i class="material-icons prefix">email</i>
+	          <input placeholder="john.doe@mail.com" id="email" name="email" type="email" class="validate" required>
+	          <label for="email">Votre adresse mail</label>
+	        </div>
+	      </div>
+	      <div class="row">
+	        <div class="input-field col s12">
+	          <i class="material-icons prefix">subject</i>
+	          <input placeholder="Objet de votre message" id="subject" name="objet" type="text" class="validate" required>
+	          <label for="subject">Sujet</label>
+	        </div>
+	      </div>
+	      <div class="row">
+	        <div class="input-field col s12">
+	          <i class="material-icons prefix">textsms</i>
+	          <textarea placeholder="Votre message" id="message" name="message" type="text" class="materialize-textarea" required></textarea>
+	          <label for="message">Votre message</label>
+	        </div>
+	      </div>
+	      <div class="row">
+	        <div class="col s12 center-align">
+	          <button class="btn waves-effect waves-light cyan darken-3" id="submit" type="submit" name="submit">Envoyer
+	            <i class="material-icons right">send</i>
+	          </button>
+	        </div>
+	      </div>
+	      <div id="feedback">
+	        <?php echo $feedback; ?>
+	      </div>
+	    </form>
+	
+	  </div>
+	</section><!--SECTION CONTACT FORM-->
+   <footer class="page-footer cyan darken-2 ">
           <div class="container  ">
             <div class="row ">
               <div class=" col m4 s12 center-align">
                 <h5 class="center-align white-text">Contacts</h5>
                 <div class="separator col s2 offset-s5 divider"></div>
                 <div class="col s12 xl6 center-align">
-               	 <img src="assets/lio.png" alt="Lionel développeur web" width="60px;"><p class="center-align grey-text text-lighten-4">lionel@cooldev.xyz</p>
+               	 <img src="assets/lio.png" alt="Lionel développeur web" width="60px;"><p><a class="center-align grey-text text-lighten-4" href="mailto:lionel@cooldev.xyz">lionel@cooldev.xyz</a></p>
                 </div>
                 <div class="col s12 xl6 center-align ">
-               	<img src="assets/mi.png" alt="Mylène chargée de projets web" width="60px;"><p class="center-align grey-text text-lighten-4">mylene@cooldev.xyz</p>
+               	<img src="assets/mi.png" alt="Mylène chargée de projets web" width="60px;"><p><a class="center-align grey-text text-lighten-4" href="mailto:mylene@cooldev.xyz">mylene@cooldev.xyz</a></p>
                 </div>
-              </div>
+              </div><!--CONTACT MAIL-->
               <div class="center-align social col m4  s12">
                <h5 class="white-text">Réseaux sociaux </h5>
 
@@ -260,17 +235,14 @@
                  <a href="https://www.facebook.com/cooldevcrew" target="_blank" ><img src="assets/logo-fb.png" alt="Suivez-nous sur Facebook"></a>
                  <a href="https://twitter.com/cooldev_crew" target="_blank" ><img src="assets/logo-twitter.png" alt="Suivez-nous sur Twitter"></a>
                 </div>
-
-
-
-              </div>
+               </div><!--RESEAUX SOCIAUX-->
               <div class="center-align col m4 s12">
                 <h5 class="white-text">Adresse</h5>
                 <div class="separator col s2 offset-s5 divider"></div>
                <div class="col s12"><p class="grey-text text-lighten-4">Cosens<br>2, rue de Rome<br>13001 Marseille</p></div>
-               </div>
+               </div><!--ADRESSE-->
             </div>
-          </div>
+          </div><!--CONTACTS-->
           <div class="footer-copyright grey-text ">
             <div class="container ">
            <span class="cyan-text text-lighten-3">CC BY-NC-SA 4.0 - 2017 /</span> <a class="cyan-text text-lighten-3" href="http://www.cooldev.xyz" target="_blank">La Team Cooldev</a>
@@ -288,10 +260,10 @@
                 <div class="modal-footer ">
                   <a href="#!" class="modal-action modal-close btn-floating "><i class="material-icons ">highlight_off</i></a>
                 </div>
-              </div>
+              </div><!--MODAL-->
             </div>
-          </div>
-        </footer>
+          </div><!--COPYRIGHT-->
+        </footer><!--FOOTER-->
     <script
 			  src="https://code.jquery.com/jquery-3.2.1.min.js"
 			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
